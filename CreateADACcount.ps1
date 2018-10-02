3d9f2c063e4f23ff413920251852dec3311b5511
r360rn <reb0rn@inbox.ru>
2018-10-02 16:44:32 +0400

    Commit
    Script creates username by taking first letter in users name and entire surname. Example: John Smith will be "JSmith@contoso.com"

---
+++ CreateADACcount.ps1
@@ -0,0 +1,11 @@
+﻿$domainname = "" # Enter Domain Name like contoso.com
+$OU = "" # Enter Organizational unit path. Example: "OU=Users, DC=contoso, DC=com"
+$name = Read-Host -Prompt 'Enter user name: '
+$surname = Read-Host -Prompt 'Enter user surname: '
+$username = $name.substring(0,1) + $surname
+$password = 'Qwerty123!' | ConvertTo-SecureString -AsPlainText -Force
+New-ADUser -Name $surname', '$name -GivenName $name -samaccountname $username -Surname $surname -userprincipalname $username + "@" + $domainname  -DisplayName $surname', '$name -Path $OU -AccountPassword $Password -ChangePasswordAtLogon $True -Enabled $True
+echo ' '
+echo ' '
+$message = 'User ' + $username + '@' + $domainname + ' with password "Qwerty123!" created.'
+write-host $message
